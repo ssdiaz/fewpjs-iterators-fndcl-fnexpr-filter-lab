8d3aa03965adfadc2969828968551edcf43465f6
Samantha Diaz <samidiaz3@gmail.com>
2022-01-28 08:55:41 -0500

    commit tree b069cdff868d17b52fea450c9ddb0fe44f248e59

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+// Write a Function To Case-insensitively Match strings
+    function findMatching(drivers, string) {
+        return drivers.filter( driver =>
+            driver.toLowerCase() === string.toLowerCase()
+        )
+    }
+    // SOLUTION
+    // function findMatching(source, sought) {
+    //     return source.filter( possibleMatch =>
+    //       possibleMatch.toLowerCase() === sought.toLowerCase()
+    //     )
+    //  }
+    
+// Write a Function To Partially Match Substrings
+    function fuzzyMatch(drivers, string) {
+        return drivers.filter( driver =>
+            driver.split('')[0] === string.split('')[0]
+        )
+    }
+    // SOLUTION
+    // function fuzzyMatch(source, testString) {
+    //     return source.filter( possibleMatch =>
+    //       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    //     )
+    //   }
+
+// Write a Function To Match object Values To a Provided string
+    function matchName(drivers, string) {
+        return drivers.filter( driver =>
+            driver.name.toLowerCase() === string.toLowerCase()
+        )
+    }
